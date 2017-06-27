1fb41cea182e235cce9d30f71801d1ebeddfbc6e
Fumiya Takahashi <fumiya.takahashi.sys@gmail.com>
2017-06-27 11:33:47 +0900

    farst commit

---
+++ src/Quiz.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Quiz {
+
+
+	public static void main(String[] args){
+		int situ1 = situmon1();
+		int situ2 = situmon2();
+		int situ3 = situmon3();
+		int situ4 = situmon4();
+		int situ5 = situmon5();
+		int situ6 = situmon6();
+
+
+	}
+
+
+
+	public static int situmon1(){
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		return ans;
+
+	}
+
+	public static int situmon2(){
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		return ans;
+
+	}
+
+	public static int situmon3(){
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		return ans;
+
+	}
+
+	public static int situmon4(){
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		return ans;
+
+	}
+
+	public static int situmon5(){
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		return ans;
+
+	}
+
+	public static int situmon6(){
+		Scanner sc = new Scanner(System.in);
+		int ans = 0;
+		return ans;
+
+	}
+
+}
